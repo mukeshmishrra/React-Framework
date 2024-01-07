f6d8e8de0fc0cf2b28c69a7514626c4910cea4d5
mukesh@campionsoftware.com <mukesh@campionsoftware.com>
2024-01-08 00:20:53 +0530

    tailwind practice app

---
+++ Projects/P4-Tailwind-Props/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Card from './Card'
+import Image1 from './assets/1.jpeg'
+
+function App() {
+
+  return (
+    <>
+    <div className='container-grid'>
+      <Card name="Note 8 Pro" />
+      <Card name="Samsung"  />
+      <Card name="Realme" />
+      <Card />
+
+    </div>
+    </>
+  )
+}
+
+export default App
